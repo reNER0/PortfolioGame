a4947c8c1d1c7525e2ca4483df7b2937964366af
Nero M3 Pro <gumerov.renat.rus@mail.ru>
2025-09-05 20:35:56 +0300

    add player model

---
+++ Assets/Scripts/Animations/PlayerAnimationController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+    [SerializeField]
+    private Animator animator;
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", player.Rigidbody.velocity.magnitude / player.GetMaxSpeed());
+    }
+}
