ce6e8985e3803cc131fbf1fde37fb922ce1855bd
Nero M3 Pro <gumerov.renat.rus@mail.ru>
2025-09-14 17:18:40 +0300

    add objects sync on start

---
+++ Assets/Scripts/PredictablesSyncOnStart.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using UnityEngine;
+
+public class PredictablesSyncOnStart : MonoBehaviour
+{
+    private void Awake()
+    {
+        var predictables = FindObjectsOfType<Predictable>()
+                                 .OrderBy(p => p.transform.GetSiblingIndex())
+                                 .ToArray();
+
+        foreach (var predictable in predictables)
+        {
+            var networkObject = new NetworkObject(predictable);
+            NetworkRepository.NetworkObjectById.Add(networkObject);
+        }
+    }
+}
